63367db800e317c4ced6b1a04687dfc6da22ac8b
tjtharrison <gh@tjth.co>
2023-06-10 22:07:31 +0100

    feat: Code refactor and docstring standardisation, added CI

--- mode_validate.py
+++ mode_validate.py
@@ -1,12 +1,29 @@
+"""Process provided mode and verify against accepted modes."""
 import sys
 
-accepted_modes = ["update","overwrite"]
 
-if len(sys.argv) < 2:
-    print("Requires input")
-else:
-    if sys.argv[1] not in accepted_modes:
-        print("Provided value " + str(sys.argv[1]) + " not in " + str(accepted_modes))
-        sys.exit(1)
+def main(args):
+    """
+    Process provided mode and verify against accepted modes.
+
+    Args:
+        args: List of arguments provided to the script
+
+    Returns:
+        True if mode is valid, exits with error code 1 if not
+    """
+    accepted_modes = ["update", "overwrite"]
+
+    if len(args) < 2:
+        print("Requires input")
     else:
-        print(sys.argv[1] + " mode set")
+        if sys.argv[1] not in accepted_modes:
+            print("Provided value " + str(sys.argv[1]) + " not in " + str(accepted_modes))
+            sys.exit(1)
+        else:
+            print(sys.argv[1] + " mode set")
+
+    return True
+
+if __name__ == "__main__":
+    main(sys.argv)
